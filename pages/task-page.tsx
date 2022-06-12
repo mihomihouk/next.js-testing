cff4ceb8f3642a2e08cc7f15d3e63a7ec9c1cd42
mihomihouk <mihomihouk@gmail.com>
2022-06-12 18:18:52 +0100

    fix error msg

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -22,7 +22,7 @@ const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
     fallbackData: staticTasks,
     revalidateOnMount: true,
   })
-  if (error) return <span>Eror!</span>
+  if (error) return <span>Error!</span>
   return (
     <Layout title="Todos">
       <p className="text-4xl m-10">todos page</p>
